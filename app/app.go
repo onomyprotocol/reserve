9dd744d40eb0bfcd95880abb014e7726e91f7400
“DongLieu” <duongduydongtq@gmail.com>
2024-09-10 10:17:48 +0700

    minor

--- app/app.go
+++ app/app.go
@@ -326,7 +326,7 @@ func New(
 	app.App = appBuilder.Build(db, traceStore, baseAppOptions...)
 
 	// Register legacy modules
-	if err := app.registerIBCModules(appOpts); err != nil {
+	if err := app.registerIBCModules(); err != nil {
 		return nil, err
 	}
 
