d0b518649f6ba5d58e9109eaf6117af110a35225
hungngohihihi <hungngohihihi@gmail.com>
2024-09-26 15:37:59 +0700

    fix test getPrice

--- x/oracle/keeper/band_oracle_test.go
+++ x/oracle/keeper/band_oracle_test.go
@@ -11,127 +11,127 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// func TestBandPriceState(t *testing.T) {
-// 	app := app.Setup(t, false)
-// 	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "3", Time: time.Unix(1618997040, 0)})
+func TestBandPriceState(t *testing.T) {
+	app := app.Setup(t, false)
+	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "3", Time: time.Unix(1618997040, 0)})
 
-// 	// Band price state is nil now
-// 	data := app.OracleKeeper.GetBandPriceState(ctx, "ATOM")
-// 	require.Nil(t, data)
+	// Band price state is nil now
+	data := app.OracleKeeper.GetBandPriceState(ctx, "ATOM")
+	require.Nil(t, data)
 
-// 	states := app.OracleKeeper.GetAllBandPriceStates(ctx)
-// 	require.Equal(t, 0, len(states))
+	states := app.OracleKeeper.GetAllBandPriceStates(ctx)
+	require.Equal(t, 0, len(states))
 
-// 	price := app.OracleKeeper.GetPrice(ctx, "ATOM", "USD")
-// 	require.Nil(t, price)
+	price := app.OracleKeeper.GetPrice(ctx, "ATOM", "USD")
+	require.Nil(t, price)
 
-// 	bandPriceState := &types.BandPriceState{
-// 		Symbol: "ATOM",
-// 		Rate:        math.NewInt(10),
-// 		ResolveTime: 1,
-// 		Request_ID:  1,
-// 		PriceState:  *types.NewPriceState(math.LegacyNewDec(10), 1),
-// 	}
-// 	// set band price state for ATOM
-// 	err := app.OracleKeeper.SetBandPriceState(ctx, "ATOM", bandPriceState)
-// 	require.NoError(t, err)
+	bandPriceState := &types.BandPriceState{
+		Symbol: "ATOM",
+		Rate:        math.NewInt(10),
+		ResolveTime: 1,
+		Request_ID:  1,
+		PriceState:  *types.NewPriceState(math.LegacyNewDec(10), 1),
+	}
+	// set band price state for ATOM
+	err := app.OracleKeeper.SetBandPriceState(ctx, "ATOM", bandPriceState)
+	require.NoError(t, err)
 
-// 	data = app.OracleKeeper.GetBandPriceState(ctx, "ATOM")
-// 	require.Equal(t, bandPriceState, data)
+	data = app.OracleKeeper.GetBandPriceState(ctx, "ATOM")
+	require.Equal(t, bandPriceState, data)
 
-// 	price = app.OracleKeeper.GetPrice(ctx, "ATOM", "USD")
-// 	expect := math.LegacyNewDec(10)
-// 	require.Equal(t, &expect, price)
+	price = app.OracleKeeper.GetPrice(ctx, "ATOM", "USD")
+	expect := math.LegacyNewDec(10)
+	require.Equal(t, &expect, price)
 
-// 	states = app.OracleKeeper.GetAllBandPriceStates(ctx)
-// 	require.Equal(t, 1, len(states))
-// }
+	states = app.OracleKeeper.GetAllBandPriceStates(ctx)
+	require.Equal(t, 1, len(states))
+}
 
-// func TestBandOracleRequest(t *testing.T) {
-// 	app := app.Setup(t, false)
-// 	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "3", Time: time.Unix(1618997040, 0)})
+func TestBandOracleRequest(t *testing.T) {
+	app := app.Setup(t, false)
+	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "3", Time: time.Unix(1618997040, 0)})
 
-// 	req := app.OracleKeeper.GetBandOracleRequest(ctx, 1)
-// 	require.Nil(t, req)
+	req := app.OracleKeeper.GetBandOracleRequest(ctx, 1)
+	require.Nil(t, req)
 
-// 	reqs := app.OracleKeeper.GetAllBandOracleRequests(ctx)
-// 	require.Equal(t, 0, len(reqs))
+	reqs := app.OracleKeeper.GetAllBandOracleRequests(ctx)
+	require.Equal(t, 0, len(reqs))
 
-// 	bandOracleRequest := types.BandOracleRequest{
-// 		RequestId:      1,
-// 		OracleScriptId: 1,
-// 		Symbols:        []string{"INJ"},
-// 		AskCount:       1,
-// 		MinCount:       1,
-// 		FeeLimit:       sdk.Coins{sdk.NewInt64Coin("INJ", 1)},
-// 		PrepareGas:     100,
-// 		ExecuteGas:     200,
-// 	}
-// 	err := app.OracleKeeper.SetBandOracleRequest(ctx, bandOracleRequest)
-// 	require.NoError(t, err)
+	bandOracleRequest := types.BandOracleRequest{
+		RequestId:      1,
+		OracleScriptId: 1,
+		Symbols:        []string{"INJ"},
+		AskCount:       1,
+		MinCount:       1,
+		FeeLimit:       sdk.Coins{sdk.NewInt64Coin("INJ", 1)},
+		PrepareGas:     100,
+		ExecuteGas:     200,
+	}
+	err := app.OracleKeeper.SetBandOracleRequest(ctx, bandOracleRequest)
+	require.NoError(t, err)
 
-// 	req = app.OracleKeeper.GetBandOracleRequest(ctx, 1)
-// 	require.Equal(t, &bandOracleRequest, req)
-// 	reqs = app.OracleKeeper.GetAllBandOracleRequests(ctx)
-// 	require.Equal(t, 1, len(reqs))
+	req = app.OracleKeeper.GetBandOracleRequest(ctx, 1)
+	require.Equal(t, &bandOracleRequest, req)
+	reqs = app.OracleKeeper.GetAllBandOracleRequests(ctx)
+	require.Equal(t, 1, len(reqs))
 
-// 	// delete request and try again
-// 	err = app.OracleKeeper.DeleteBandOracleRequest(ctx, 1)
-// 	require.NoError(t, err)
-// 	reqs = app.OracleKeeper.GetAllBandOracleRequests(ctx)
-// 	require.Equal(t, 0, len(reqs))
-// }
+	// delete request and try again
+	err = app.OracleKeeper.DeleteBandOracleRequest(ctx, 1)
+	require.NoError(t, err)
+	reqs = app.OracleKeeper.GetAllBandOracleRequests(ctx)
+	require.Equal(t, 0, len(reqs))
+}
 
-// func TestBandLatestClientId(t *testing.T) {
-// 	app := app.Setup(t, false)
-// 	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "3", Time: time.Unix(1618997040, 0)})
+func TestBandLatestClientId(t *testing.T) {
+	app := app.Setup(t, false)
+	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "3", Time: time.Unix(1618997040, 0)})
 
-// 	id := app.OracleKeeper.GetBandLatestClientID(ctx)
-// 	require.Equal(t, uint64(0), id)
+	id := app.OracleKeeper.GetBandLatestClientID(ctx)
+	require.Equal(t, uint64(0), id)
 
-// 	err := app.OracleKeeper.SetBandLatestClientID(ctx, 10)
-// 	require.NoError(t, err)
+	err := app.OracleKeeper.SetBandLatestClientID(ctx, 10)
+	require.NoError(t, err)
 
-// 	id = app.OracleKeeper.GetBandLatestClientID(ctx)
-// 	require.Equal(t, uint64(10), id)
-// }
+	id = app.OracleKeeper.GetBandLatestClientID(ctx)
+	require.Equal(t, uint64(10), id)
+}
 
-// func TestBandLatestRequestId(t *testing.T) {
-// 	app := app.Setup(t, false)
-// 	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "3", Time: time.Unix(1618997040, 0)})
+func TestBandLatestRequestId(t *testing.T) {
+	app := app.Setup(t, false)
+	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "3", Time: time.Unix(1618997040, 0)})
 
-// 	id := app.OracleKeeper.GetBandLatestRequestID(ctx)
-// 	require.Equal(t, uint64(0), id)
+	id := app.OracleKeeper.GetBandLatestRequestID(ctx)
+	require.Equal(t, uint64(0), id)
 
-// 	err := app.OracleKeeper.SetBandLatestRequestID(ctx, 1)
-// 	require.NoError(t, err)
+	err := app.OracleKeeper.SetBandLatestRequestID(ctx, 1)
+	require.NoError(t, err)
 
-// 	id = app.OracleKeeper.GetBandLatestRequestID(ctx)
-// 	require.Equal(t, uint64(1), id)
-// }
+	id = app.OracleKeeper.GetBandLatestRequestID(ctx)
+	require.Equal(t, uint64(1), id)
+}
 
-// func TestBandCallDataRecord(t *testing.T) {
-// 	app := app.Setup(t, false)
-// 	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "3", Time: time.Unix(1618997040, 0)})
+func TestBandCallDataRecord(t *testing.T) {
+	app := app.Setup(t, false)
+	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "3", Time: time.Unix(1618997040, 0)})
 
-// 	record := app.OracleKeeper.GetBandCallDataRecord(ctx, 1)
-// 	require.Nil(t, record)
+	record := app.OracleKeeper.GetBandCallDataRecord(ctx, 1)
+	require.Nil(t, record)
 
-// 	recordA := &types.CalldataRecord{
-// 		ClientId: 1,
-// 		Calldata: []byte("123"),
-// 	}
-// 	err := app.OracleKeeper.SetBandCallDataRecord(ctx, recordA)
-// 	require.NoError(t, err)
-// 	record = app.OracleKeeper.GetBandCallDataRecord(ctx, 1)
-// 	require.Equal(t, recordA, record)
+	recordA := &types.CalldataRecord{
+		ClientId: 1,
+		Calldata: []byte("123"),
+	}
+	err := app.OracleKeeper.SetBandCallDataRecord(ctx, recordA)
+	require.NoError(t, err)
+	record = app.OracleKeeper.GetBandCallDataRecord(ctx, 1)
+	require.Equal(t, recordA, record)
 
-// 	err = app.OracleKeeper.DeleteBandCallDataRecord(ctx, 1)
-// 	require.NoError(t, err)
+	err = app.OracleKeeper.DeleteBandCallDataRecord(ctx, 1)
+	require.NoError(t, err)
 
-// 	record = app.OracleKeeper.GetBandCallDataRecord(ctx, 1)
-// 	require.Nil(t, record)
-// }
+	record = app.OracleKeeper.GetBandCallDataRecord(ctx, 1)
+	require.Nil(t, record)
+}
 
 func TestGetPrice(t *testing.T) {
 	app := app.Setup(t, false)
