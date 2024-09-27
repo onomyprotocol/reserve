04c449095a7febbc5ef20ea00a5714130c05fe32
hungngohihihi <hungngohihihi@gmail.com>
2024-09-27 15:03:26 +0700

    add test ProcessBandOraclePrices

--- x/oracle/keeper/band_oracle_test.go
+++ x/oracle/keeper/band_oracle_test.go
@@ -3,12 +3,14 @@ package keeper_test
 import (
 	"testing"
 	"time"
+
 	"cosmossdk.io/math"
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
-	"github.com/onomyprotocol/reserve/app"
-	"github.com/stretchr/testify/require"
-	"github.com/onomyprotocol/reserve/x/oracle/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/onomyprotocol/reserve/app"
+	"github.com/onomyprotocol/reserve/x/oracle/types"
+	"github.com/onomyprotocol/reserve/x/oracle/utils"
+	"github.com/stretchr/testify/require"
 )
 
 func TestBandPriceState(t *testing.T) {
@@ -26,7 +28,7 @@ func TestBandPriceState(t *testing.T) {
 	require.Nil(t, price)
 
 	bandPriceState := &types.BandPriceState{
-		Symbol: "ATOM",
+		Symbol:      "ATOM",
 		Rate:        math.NewInt(10),
 		ResolveTime: 1,
 		Request_ID:  1,
@@ -50,7 +52,7 @@ func TestBandPriceState(t *testing.T) {
 func TestBandOracleRequest(t *testing.T) {
 	app := app.Setup(t, false)
 	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "3", Time: time.Unix(1618997040, 0)})
-	
+
 	req := app.OracleKeeper.GetBandOracleRequest(ctx, 1)
 	require.Nil(t, req)
 
@@ -131,4 +133,116 @@ func TestBandCallDataRecord(t *testing.T) {
 
 	record = app.OracleKeeper.GetBandCallDataRecord(ctx, 1)
 	require.Nil(t, record)
+}
+
+func TestProcessBandOraclePrices(t *testing.T) {
+	// Set up the application and context
+	app := app.Setup(t, false)
+	ctx := app.BaseApp.NewContextLegacy(false, tmproto.Header{Height: 1, ChainID: "3", Time: time.Unix(1618997040, 0)})
+
+	// Define table-driven test cases
+	tests := []struct {
+		name          string
+		clientID      string
+		calldata      *types.CalldataRecord
+		oracleOutput  interface{}
+		expectedError bool
+		expectedRate  int64
+	}{
+		{
+			name:          "Fail when no CallDataRecord found",
+			clientID:      "1",
+			calldata:      nil,
+			oracleOutput:  nil,
+			expectedError: false,
+		},
+		{
+			name:     "Fail when decoding OracleInput",
+			clientID: "1",
+			calldata: &types.CalldataRecord{
+				ClientId: 1,
+				Calldata: []byte{0xFF, 0xFF},
+			},
+			oracleOutput:  nil,
+			expectedError: true,
+		},
+		{
+			name:     "Fail when decoding OracleOutput",
+			clientID: "1",
+			calldata: &types.CalldataRecord{
+				ClientId: 1,
+				Calldata: utils.MustEncode(types.Input{
+					Symbols:    []string{"ATOM", "BTC"},
+					Multiplier: types.BandPriceMultiplier,
+				}),
+			},
+			oracleOutput:  []byte{0xFF, 0xFF},
+			expectedError: true,
+		},
+		{
+			name:     "Success with valid OracleResponsePacketData",
+			clientID: "1",
+			calldata: &types.CalldataRecord{
+				ClientId: 1,
+				Calldata: utils.MustEncode(types.Input{
+					Symbols:    []string{"ATOM", "BTC"},
+					Multiplier: types.BandPriceMultiplier,
+				}),
+			},
+			oracleOutput: types.BandOutput{
+				Responses: []types.Response{
+					{Symbol: "ATOM", ResponseCode: 0, Rate: 100},
+					{Symbol: "BTC", ResponseCode: 0, Rate: 50000},
+				},
+			},
+			expectedError: false,
+			expectedRate:  100,
+		},
+	}
+
+	// Iterate over each test case
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.calldata != nil {
+				err := app.OracleKeeper.SetBandCallDataRecord(ctx, tt.calldata)
+				require.NoError(t, err)
+			}
+
+			var result []byte
+			if tt.oracleOutput != nil {
+				switch v := tt.oracleOutput.(type) {
+				case types.BandOutput:
+					result = utils.MustEncode(v)
+				case []byte:
+					result = v
+				}
+			}
+
+			oraclePacketData := types.OracleResponsePacketData{
+				ClientID:    tt.clientID,
+				RequestID:   1,
+				Result:      result,
+				ResolveTime: time.Now().Unix(),
+			}
+
+			relayer := sdk.AccAddress("mock-relayer-address")
+
+			err := app.OracleKeeper.ProcessBandOraclePrices(ctx, relayer, oraclePacketData)
+
+			if tt.expectedError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+
+				record := app.OracleKeeper.GetBandCallDataRecord(ctx, 1)
+				require.Nil(t, record)
+
+				if tt.expectedRate != 0 {
+					priceState := app.OracleKeeper.GetBandPriceState(ctx, "ATOM")
+					require.NotNil(t, priceState)
+					require.Equal(t, tt.expectedRate, priceState.Rate.Int64())
+				}
+			}
+		})
+	}
 }
