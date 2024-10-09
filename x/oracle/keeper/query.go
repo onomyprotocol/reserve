6b211f4f60f4f129daa83fad13ef05073995c2a1
ThanhNhann <thanhnhan98qh@gmail.com>
2024-10-09 14:43:18 +0700

    debug

--- x/oracle/keeper/query.go
+++ x/oracle/keeper/query.go
@@ -10,7 +10,11 @@ var _ types.QueryServer = Keeper{}
 
 func (k Keeper) BandPriceStates(c context.Context, _ *types.QueryBandPriceStatesRequest) (*types.QueryBandPriceStatesResponse, error) {
 	ctx := sdk.UnwrapSDKContext(c)
-
+	println("Check query band prices state .......")
+	data := k.GetAllBandPriceStates(ctx)
+	for _, state := range data {
+		println("check state: ", state.String())
+	}
 	res := &types.QueryBandPriceStatesResponse{
 		PriceStates: k.GetAllBandPriceStates(ctx),
 	}
