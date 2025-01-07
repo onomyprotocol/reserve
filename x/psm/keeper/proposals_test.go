01ff2ebb357abfb853bbc3874aa6efe8c216a827
Đông Liều <duongduydongtq@gmail.com>
2025-01-07 13:12:52 +0700

    add new test

--- x/psm/keeper/proposals_test.go
+++ x/psm/keeper/proposals_test.go
@@ -3,6 +3,7 @@ package keeper_test
 import (
 	"cosmossdk.io/math"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/onomyprotocol/reserve/x/psm/types"
@@ -54,6 +55,35 @@ func (s *KeeperTestSuite) TestAddStableCoinProposal() {
 	coinsStable := s.App.BankKeeper.GetAllBalances(s.Ctx, s.App.AccountKeeper.GetModuleAddress(types.ModuleName))
 	s.Require().Equal(coinsStable.AmountOf(usdt), amountStableInit)
 	s.Require().Equal(coinsStable.AmountOf(usdc), amountStableInit)
+}
+
+func (s *KeeperTestSuite) TestAddStableCoinProposalFromAnyAddress() {
+	s.SetupTest()
+
+	s.FundAccount(s.TestAccs[2], types.ModuleName, sdk.NewCoins(sdk.NewCoin(usdt, amountStableInit)))
+
+	proAdd := types.MsgAddStableCoin{
+		Denom:                usdt,
+		AddressPayStableInit: s.TestAccs[2].String(),
+		AmountStableInit:     amountStableInit,
+		LimitTotal:           limitUSDT,
+		Symbol:               usdt,
+		Authority:            authtypes.NewModuleAddress(govtypes.ModuleName).String(),
+		FeeIn:                math.LegacyMustNewDecFromStr("0.001"),
+		FeeOut:               math.LegacyMustNewDecFromStr("0.001"),
+		OracleScript:         44,
+	}
+
+	_, err := s.msgServer.AddStableCoinProposal(s.Ctx, &proAdd)
+	s.Require().NoError(err)
+
+	stablecoin, err := s.k.StablecoinInfos.Get(s.Ctx, proAdd.Denom)
+	s.Require().NoError(err)
+	s.Require().Equal(stablecoin.Denom, proAdd.Denom)
+	s.Require().Equal(stablecoin.LimitTotal, limitUSDT)
+
+	coinsStable := s.App.BankKeeper.GetAllBalances(s.Ctx, s.App.AccountKeeper.GetModuleAddress(types.ModuleName))
+	s.Require().Equal(coinsStable.AmountOf(usdt), amountStableInit)
 }
 
 func (s *KeeperTestSuite) TestUpdateStableCoinProposal() {
