0db49d6e2a03d3d427a9d966fa0befe86a7b059a
Đông Liều <duongduydongtq@gmail.com>
2025-01-21 14:30:39 +0700

    updates test TestBurnShortfallByMintDenom

--- x/vaults/keeper/msg_server_test.go
+++ x/vaults/keeper/msg_server_test.go
@@ -39,6 +39,88 @@ func (s *KeeperTestSuite) TestBurnShortfallByMintDenom() {
 			expReserveBalcesAfterBurn:   math.NewInt(9_000_000),
 			expPass:                     true,
 		},
+		{
+			name:      "success, reserve balancess is less than shortfallAmount",
+			mintDenom: "fxUSD",
+			setup: func() types.MsgBurnShortfall {
+				// make sure reserve has money
+				mintCoin := sdk.NewCoins(sdk.NewCoin("fxUSD", math.NewInt(1_000_000)))
+				s.FundAccount(s.TestAccs[0], types.ModuleName, mintCoin)
+				err := s.k.BankKeeper.SendCoinsFromAccountToModule(s.Ctx, s.TestAccs[0], types.ReserveModuleName, mintCoin)
+				s.Require().NoError(err)
+
+				// make sure Guaranteed Shortfall Amount
+				err = s.k.ShortfallAmount.Set(s.Ctx, "fxUSD", math.NewInt(10_000_000))
+				s.Require().NoError(err)
+				return types.MsgBurnShortfall{
+					Authority: "onomy10d07y265gmmuvt4z0w9aw880jnsr700jqr8n8k",
+					MintDenom: "fxUSD",
+				}
+			},
+			expShortfallAmountAfterBurn: math.NewInt(9_000_000),
+			expReserveBalcesAfterBurn:   math.ZeroInt(),
+			expPass:                     true,
+		},
+		{
+			name:      "success, reserve balancess no money",
+			mintDenom: "fxUSD",
+			setup: func() types.MsgBurnShortfall {
+				// make sure Guaranteed Shortfall Amount
+				err := s.k.ShortfallAmount.Set(s.Ctx, "fxUSD", math.NewInt(10_000_000))
+				s.Require().NoError(err)
+				return types.MsgBurnShortfall{
+					Authority: "onomy10d07y265gmmuvt4z0w9aw880jnsr700jqr8n8k",
+					MintDenom: "fxUSD",
+				}
+			},
+			expShortfallAmountAfterBurn: math.NewInt(10_000_000),
+			expReserveBalcesAfterBurn:   math.ZeroInt(),
+			expPass:                     true,
+		},
+		{
+			name:      "fail, government account not the signatory for the proposed message",
+			mintDenom: "fxUSD",
+			setup: func() types.MsgBurnShortfall {
+				// make sure reserve has money
+				mintCoin := sdk.NewCoins(sdk.NewCoin("fxUSD", math.NewInt(1_000_000)))
+				s.FundAccount(s.TestAccs[0], types.ModuleName, mintCoin)
+				err := s.k.BankKeeper.SendCoinsFromAccountToModule(s.Ctx, s.TestAccs[0], types.ReserveModuleName, mintCoin)
+				s.Require().NoError(err)
+
+				// make sure Guaranteed Shortfall Amount
+				err = s.k.ShortfallAmount.Set(s.Ctx, "fxUSD", math.NewInt(10_000_000))
+				s.Require().NoError(err)
+				return types.MsgBurnShortfall{
+					Authority: s.TestAccs[0].String(),
+					MintDenom: "fxUSD",
+				}
+			},
+			expShortfallAmountAfterBurn: math.NewInt(9_000_000),
+			expReserveBalcesAfterBurn:   math.ZeroInt(),
+			expPass:                     false,
+		},
+		{
+			name:      "fail, denom is not in the allowed mint denom list",
+			mintDenom: "fxUSD",
+			setup: func() types.MsgBurnShortfall {
+				// make sure reserve has money
+				mintCoin := sdk.NewCoins(sdk.NewCoin("fxUSD", math.NewInt(1_000_000)))
+				s.FundAccount(s.TestAccs[0], types.ModuleName, mintCoin)
+				err := s.k.BankKeeper.SendCoinsFromAccountToModule(s.Ctx, s.TestAccs[0], types.ReserveModuleName, mintCoin)
+				s.Require().NoError(err)
+
+				// make sure Guaranteed Shortfall Amount
+				err = s.k.ShortfallAmount.Set(s.Ctx, "fxUSD", math.NewInt(10_000_000))
+				s.Require().NoError(err)
+				return types.MsgBurnShortfall{
+					Authority: s.TestAccs[0].String(),
+					MintDenom: "atom",
+				}
+			},
+			expShortfallAmountAfterBurn: math.NewInt(9_000_000),
+			expReserveBalcesAfterBurn:   math.ZeroInt(),
+			expPass:                     false,
+		},
 	}
 
 	for _, t := range testcases {
